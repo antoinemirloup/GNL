3554fa59b9dcd8e4178c7b3f248312bbdb3a3552
Antoine Mirloup <amirloup@k0r3p4.42mulhouse.fr>
2023-11-14 15:51:32 +0100

    8th push

---
+++ get_next_line_bonus.c
@@ -0,0 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amirloup <amirloup@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 15:16:54 by amirloup          #+#    #+#             */
+/*   Updated: 2023/11/14 15:44:46 by amirloup         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+static char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != (unsigned char)c)
+	{
+		if (s[i] == '\0')
+			return ((char *)0);
+		i++;
+	}
+	return ((char *)&s[i]);
+}
+
+static char	*ft_read(char *line)
+{
+	int		i;
+	char	*new_line;
+
+	i = 0;
+	if (!line || !line[0])
+	{
+		free(line);
+		return (NULL);
+	}
+	while (line[i] != '\0' && line[i] != '\n')
+		i++;
+	new_line = malloc((i + 2) * sizeof(char));
+	if (!new_line)
+		return (NULL);
+	i = 0;
+	while (line[i] != '\0' && line[i] != '\n')
+	{
+		new_line[i] = line[i];
+		i++;
+	}
+	if (line[i] == '\n')
+		new_line[i++] = '\n';
+	new_line[i] = '\0';
+	free(line);
+	return (new_line);
+}
+
+static void	ft_sort(char *buffer)
+{
+	int	i;
+
+	i = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	ft_memmove(buffer, buffer + i, (BUFFER_SIZE - i));
+	ft_bzero(&buffer[BUFFER_SIZE - i], i);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	buffer[BUFFER_SIZE + 1][1024];
+	int			cursor;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	cursor = 1;
+	line = NULL;
+	line = ft_strjoin(line, buffer);
+	while (cursor != 0 && ft_strchr(buffer, '\n') == NULL)
+	{
+		cursor = read(fd, buffer, BUFFER_SIZE);
+		if (cursor == -1)
+		{
+			ft_bzero(buffer, BUFFER_SIZE);
+			free(line);
+			return (NULL);
+		}
+		buffer[cursor] = '\0';
+		line = ft_strjoin(line, buffer);
+	}
+	line = ft_read(line);
+	ft_sort(buffer);
+	return (line);
+}
+
+// #include <fcntl.h>
+
+// int	main(void)
+// {
+// 	char	*line;
+// 	int		i;
+// 	int		fd;
+
+// 	fd = open("text.txt", O_RDONLY);
+// 	i = 1;
+// 	while (i <= 2)
+// 	{
+// 		line = get_next_line(fd);
+// 		printf("%s", line);
+// 		free(line);
+// 		i++;
+// 	}
+// 	close(fd);
+// 	fd = open("text.txt", O_RDONLY);
+// 	i = 1;
+// 	while (i <= 4)
+// 	{
+// 		line = get_next_line(fd);
+// 		printf("%s", line);
+// 		free(line);
+// 		i++;
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amirloup <amirloup@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 15:17:00 by amirloup          #+#    #+#             */
+/*   Updated: 2023/11/14 15:24:14 by amirloup         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		i;
+	int		j;
+	char	*d;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+		i++;
+	d = malloc((i + 1) * sizeof(char));
+	if (d == NULL)
+		return (NULL);
+	while (s[j] && j < i)
+	{
+		d[j] = s[j];
+		j++;
+	}
+	d[j] = '\0';
+	return (d);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t		i;
+	size_t		j;
+	char		*s;
+	size_t		sizes1;
+	size_t		sizes2;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+		return (ft_strdup(s2));
+	sizes1 = ft_strlen(s1);
+	sizes2 = ft_strlen(s2);
+	s = (char *)malloc((sizes1 + sizes2 + 1) * sizeof(char));
+	if (!s)
+		return (NULL);
+	while (s1[i])
+		s[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		s[j++] = s2[i++];
+	s[j] = '\0';
+	free(s1);
+	return (s);
+}
+
+void	*ft_memmove(char *dest, char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dest && !src)
+		return (0);
+	if (dest > src)
+	{
+		while (n > 0)
+		{
+			dest[n - 1] = src[n - 1];
+			n--;
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	return (dest);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*ptr;
+
+	ptr = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+}
